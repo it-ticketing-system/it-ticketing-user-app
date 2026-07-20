46f0a2cd2ac248ead89d60e321cc1773b69c3200
ehsanKey <ehsanKeyhani82@gmail.com>
2026-07-20 20:23:37 +0330

    feat(components): add logo component

---
+++ src/components/shared/logo.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import { FC } from 'react';
+
+const Logo: FC = () => {
+  return (
+    <Image
+      src="/logo/logo-mark-primary-512.png"
+      width={40}
+      height={40}
+      alt="Logo"
+    />
+  );
+};
+
+export default Logo;
