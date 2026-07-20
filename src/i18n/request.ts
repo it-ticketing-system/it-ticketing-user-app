927251ef9d9d3421426883b96a198a74446c13b5
ehsanKey <ehsanKeyhani82@gmail.com>
2026-07-20 20:13:11 +0330

    feat(localization): add footer, header, and home localization files and register them in request config

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,7 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
 
 import main_layout from '../../messages/fa/main-layout.json';
+import header from '../../messages/fa/header.json';
 import home from '../../messages/fa/home.json';
+import footer from '../../messages/fa/footer.json';
 
 export default getRequestConfig(async () => {
   return {
@@ -9,7 +11,9 @@ export default getRequestConfig(async () => {
 
     messages: {
       main_layout,
+      header,
       home,
+      footer,
     },
   };
 });
